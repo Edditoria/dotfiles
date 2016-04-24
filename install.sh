7d88a21b259c8d5ff4ef05e6884f9bea975c6c73
Edditoria <edditoria@gmail.com>
2016-04-25 00:02:04 +0800

    minor changes, remove Mac trackpad tap to click for login screen

--- install.sh
+++ install.sh
@@ -27,11 +27,14 @@ defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 # Disable smart dashes as they're annoying when typing code
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 
-# Trackpad: enable tap to click for this user and for the login screen
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+# # Trackpad: enable tap to click for this user and for the login screen
+# defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+# defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+# defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+# # doesn't work in Yosemite
 
 # Screeshot
 defaults write com.apple.screencapture disable-shadow -bool true
 defaults write com.apple.screencapture name "Screenshot"
+
+killall SystemUIServer
