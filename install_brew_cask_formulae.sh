f9a25b420d47b522b99511693a0c945a7eda23e4
Edditoria <edditoria@gmail.com>
2016-09-22 20:10:10 +0800

    add Caskroom fonts to manage fonts now

--- install_brew_cask_formulae.sh
+++ install_brew_cask_formulae.sh
@@ -13,6 +13,7 @@ brew install $(cat "$BACKUP_DIR/brew_leaves.txt")
 
 # add Cask repo to Homebrew
 brew tap caskroom/cask
+brew tap caskroom/fonts
 
 # install cask apps
 brew cask install $(cat "$BACKUP_DIR/brew_cask_list.txt")
