ec7f47a677f2b8406f1ea4f360809623abf14d5a
Edditoria <edditoria@gmail.com>
2018-05-05 13:31:30 +0800

    update defaults write

--- setup_macos_env.sh
+++ setup_macos_env.sh
@@ -6,17 +6,23 @@
 sudo -v
 
 # Disable the boot up "ding" sound
-sudo nvram SystemAudioVolume=" "
+# sudo nvram SystemAudioVolume=" " # not apply to High Sierra
 
 # Expand save panel by default
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true # default no value
+# defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true # testing in High Sierra
 
 # Disable smart quotes as they're annoying when typing code
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 
 # Disable smart dashes as they're annoying when typing code
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Audo-hide Dock
+defaults write com.apple.dock autohide -bool true
+
+# Disable auto-rearrange Spaces
+defaults write com.apple.dock mru-spaces -bool false # High Sierra, default no value
 
 # # Trackpad: enable tap to click for this user and for the login screen
 # defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
@@ -28,4 +34,5 @@ defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 defaults write com.apple.screencapture disable-shadow -bool true
 defaults write com.apple.screencapture name "Screenshot"
 
+killall Dock
 killall SystemUIServer
