74c3954834adfcc537b9abf1149248253225e5f1
Edditoria <edditoria@gmail.com>
2018-06-09 20:53:40 +0800

    update pyenv and rbenv setup

--- setup_pyenv.sh
+++ setup_pyenv.sh
@@ -2,16 +2,17 @@
 
 # pyenv is already installed in install_brew_cask_formulae.sh
 
-pyenv install 2.7.14
-pyenv install 3.6.3
+pyenv install 2.7.15
+pyenv install 3.6.5
 echo "[\$_\$] pyenv versions"
 pyenv versions
 echo "[\$_\$] pip --version"
 pip --version
 
+pyenv global 3.6.5
 pip install Django
 echo "[\$_\$] print django verion:"
-python -c "import django; print(django.get_version())"
+python3 -c "import django; print(django.get_version())"
 
 echo "Switch back to pyenv global system"
 pyenv global system
--- setup_rbenv.sh
+++ setup_rbenv.sh
@@ -6,9 +6,9 @@
 # !important: Need to update version manually, info: http://guides.rubyonrails.org
 
 mkdir -p "$(rbenv root)/plugins"
-rbenv install 2.3.3
+rbenv install 2.5.1
 rbenv rehash
-rbenv global 2.3.3
+rbenv global 2.5.1
 rbenv rehash
 echo "[\$_\$] rbenv versions"
 rbenv versions
@@ -25,17 +25,8 @@ rbenv which gem # /Users/Edditoria/.rbenv/versions/2.3.3/bin/gem
 echo "[\$_\$] rbenv which bundle"
 rbenv which bundle # /Users/Edditoria/.rbenv/versions/2.3.3/bin/bundle
 
-# Install nokogirl gem in different way
-# This may not fit to your case
-# os_version="$(sw_vers -productVersion | sed 's:.[[:digit:]]*.$::g')"
-# if [[ ${os_version} = "10.10" ]]; then
-#   gem install nokogiri -- --use-system-libraries=true --with-xml2-include=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk/usr/include/libxml2/
-# fi
-# FUCK YOU RAILS. FUCK YOU NOKOGIRL.
-# Anyway, the problem is solved by adding BUNDLE_BUILD__NOKOGIRI to .bundle/config
-
 rbenv rehash
-gem install rails -v 5.0.1
-rbenv rehash
-echo "[\$_\$] rails -v"
-rails -v
+# gem install rails -v 5.0.1
+# rbenv rehash
+# echo "[\$_\$] rails -v"
+# rails -v
