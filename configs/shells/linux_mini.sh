a322ed3a746241da9e695588479c6c784ae483e3
Edditoria <edditoria@pm.me>
2022-08-10 16:37:14 +0800

    Refactor configs:shells for better infomation

--- configs/shells/linux_mini.sh
+++ configs/shells/linux_mini.sh
@@ -1,29 +1,55 @@
 export DEV="$HOME/dev"
 export DOTFILES="$DEV/dotfiles"
-export HOMEBREW_CASK_OPTS="--appdir=/Applications/Cask"
 # export XDG_CONFIG_HOME="$HOME/.config"
+export HOMEBREW_CASK_OPTS="--appdir=/Applications/Cask"
+export ASDF_NPM_DEFAULT_PACKAGES_FILE="$DOTFILES/configs/node/.default-npm-packages"
+export NVM_DIR="$HOME/.nvm"
+export CHROME_EXECUTABLE='/snap/bin/chromium' # for flutter.
+export PATH="$HOME/.rbenv/bin:$PATH"
 
 # Linuxbrew aka Homebrew
-eval "$($HOME/.linuxbrew/bin/brew shellenv)"
-
-# asdf (inc: node, python, etc.)
-export ASDF_NPM_DEFAULT_PACKAGES_FILE="$DOTFILES/configs/default_npm_packages"
-if [ -f $HOME/apps/asdf/asdf.sh ]; then
-	source $HOME/apps/asdf/asdf.sh
+if [[ -f "$HOME/.linuxbrew/bin/brew" ]]; then
+	eval "$($HOME/.linuxbrew/bin/brew shellenv)"
+else
+	echo '[x_x] Cannot find Homebrew.'
 fi
 
+# asdf (inc: node, python, etc.)
+function init_asdf {
+	if [[ -f $HOME/apps/asdf/asdf.sh ]]; then
+		source $HOME/apps/asdf/asdf.sh
+	else
+		echo '[x_x] Cannot find asdf.'
+	fi
+}
+
 # nvm for node
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+function init_nvm {
+	if [[ -s "$NVM_DIR/nvm.sh" ]]; then
+		source "$NVM_DIR/nvm.sh"
+	else
+		echo '[x_x] Cannot find nvm.sh file.'
+	fi
+	if [[ -s "$NVM_DIR/bash_completion" ]]; then
+		source "$NVM_DIR/bash_completion"
+	else
+		echo '[x_x] Cannot find bash_completion of nvm.'
+	fi
+}
 
 # rbenv for ruby
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+function init_rbenv {
+	if which rbenv > /dev/null; then
+		eval "$(rbenv init -)"
+	else
+		echo '[x_x] Cannot find rbenv.'
+	fi
+}
 
 # golang
 # export PATH=$PATH:/usr/local/go/bin
 # NOTE: No need to set GOPATH and GOROOT since go1.8
 
-# flutter
-export CHROME_EXECUTABLE='/snap/bin/chromium'
+init_asdf
+# init_nvm
+init_rbenv
--- configs/shells/macos.sh
+++ configs/shells/macos.sh
@@ -1,23 +1,50 @@
 export DEV="$HOME/dev"
 export DOTFILES="$DEV/dotfiles"
 export HOMEBREW_CASK_OPTS="--appdir=/Applications/Cask"
+export ASDF_NPM_DEFAULT_PACKAGES_FILE="$DOTFILES/configs/node/.default-npm-packages"
+export NVM_DIR="$HOME/.nvm"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 
-# asdf (inc: python, etc.)
-export ASDF_NPM_DEFAULT_PACKAGES_FILE="$DOTFILES/configs/default_npm_packages"
-source $(brew --prefix asdf)/libexec/asdf.sh
-source $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash
+# asdf (includes node, python, etc.)
+function init_asdf {
+	if [[ -s $(brew --prefix asdf)/libexec/asdf.sh ]]; then
+		source $(brew --prefix asdf)/libexec/asdf.sh
+		source $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash
+	else
+		echo '[x_x] Cannot find asdf.'
+	fi
+}
 
 # nvm for node
-export NVM_DIR=$HOME/.nvm
-if [[ -s $(brew --prefix nvm)/nvm.sh ]]; then
-	source $(brew --prefix nvm)/nvm.sh
-	source $(brew --prefix nvm)/etc/bash_completion.d/nvm
-fi
+function init_nvm {
+	if [[ -s $(brew --prefix nvm)/nvm.sh ]]; then
+		source $(brew --prefix nvm)/nvm.sh
+		source $(brew --prefix nvm)/etc/bash_completion.d/nvm
+	else
+		echo '[x_x] Cannot find nvm.'
+	fi
+}
 
 # rbenv for ruby
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+function init_rbenv {
+	if which rbenv > /dev/null; then
+		eval "$(rbenv init -)"
+	else
+		echo '[x_x] Cannot find rbenv.'
+	fi
+}
 
 # pyenv for python
-# export PYENV_ROOT="$HOME/.pyenv"
-# export PATH="$PYENV_ROOT/bin:$PATH"
-# if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+function init_pyenv {
+	if which pyenv > /dev/null; then
+		eval "$(pyenv init -)"
+	else
+		echo '[x_x] Cannot find pyenv.'
+	fi
+}
+
+init_asdf
+# init_nvm
+init_rbenv
+# init_pyenv
