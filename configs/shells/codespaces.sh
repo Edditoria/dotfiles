4ac2047e434b8588c43c1a05758aedfc4e4612c5
Edditoria <edditoria@pm.me>
2022-08-17 13:50:01 +0800

    Add exporting asdf env var in shells configs for CodeSpaces

--- configs/shells/codespaces.sh
+++ configs/shells/codespaces.sh
@@ -3,6 +3,10 @@ export DOTFILES="/workspaces/.codespaces/.persistedshare/dotfiles"
 # export HOMEBREW_CASK_OPTS="--appdir=/Applications/Cask" # Too slow for adhoc up-and-running.
 # export NVM_DIR # CodesSpaces implements its own nvm called "NVS".
 
+# asdf is not installed. Get ready only:
+export ASDF_CONFIG_FILE="$DOTFILES/configs/asdf/.asdfrc"
+export ASDF_NPM_DEFAULT_PACKAGES_FILE="$DOTFILES/configs/node/.default-npm-packages"
+
 # source "$DOTFILES/configs/shells/functions/init.sh"
 
 source "$DOTFILES/configs/shells/functions/alias.sh"
