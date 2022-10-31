60279f8fc882d5eec08cb5d0aaf22f982e66fa6d
Edditoria <edditoria@pm.me>
2022-10-31 20:21:11 +0800

    Update configs:node

--- configs/node/install_lists.sh
+++ configs/node/install_lists.sh
@@ -1,16 +1,16 @@
 # Implement in functions/setup_asdf_nodejs and functions/setup_nvm
 local node_list=(
-	'lts/fermium'
 	'lts/gallium'
+	'lts/hydrogen'
 )
 
 # Implement in functions/setup_nvm
 local alias_list=(
-	'f lts/fermium'
 	'g lts/gallium'
-	'default g'
+	'h lts/hydrogen'
+	'default h'
 	'd default'
 )
 
 # Implement in functions/setup_asdf_nodejs
-local default_node='lts-gallium'
+local default_node='lts-hydrogen'
