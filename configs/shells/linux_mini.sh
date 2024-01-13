a9e9283719f485e18c3522f63161e78c384e8828
Edditoria <edditoria@pm.me>
2024-01-13 19:52:27 +0800

    Update configs:shells: ASDF-NodeJS now resolves with "latest_installed"

--- configs/shells/linux_mini.sh
+++ configs/shells/linux_mini.sh
@@ -6,6 +6,7 @@ export DISABLE_DEVICONS=false # for Neovim config.
 export HOMEBREW_CASK_OPTS="--appdir=/Applications/Cask"
 export ASDF_CONFIG_FILE="$DOTFILES/configs/asdf/.asdfrc"
 export ASDF_NPM_DEFAULT_PACKAGES_FILE="$DOTFILES/configs/node/.default-npm-packages"
+export ASDF_NODEJS_LEGACY_FILE_DYNAMIC_STRATEGY="latest_installed"
 export ASDF_PYTHON_DEFAULT_PACKAGES_FILE="$DOTFILES/configs/python/.default-python-packages"
 export CHROME_EXECUTABLE="$(which chrome || which chromium)" # for flutter.
 
